a01e66262ae98309aa25c02c71c9d2f806996425
obedewardhana <obedewardhana@gmail.com>
2022-12-08 19:39:40 +0700

    add component followers

--- src/components/Feeds/FeedStacked.vue
+++ src/components/Feeds/FeedStacked.vue
@@ -1,11 +1,12 @@
 <template>
     <v-row>
         <v-col cols="12">
-            <h2 class="primary--text px-0 px-sm-8 px-md-8 px-xl-8 text-center text-sm-center text-md-left text-lg-left">BERITA LAINNYA</h2>
+            <h2 class="primary--text px-0 px-sm-8 px-md-8 px-xl-8 text-center text-sm-center text-md-left text-lg-left">
+                KABAR LAINNYA</h2>
         </v-col>
         <v-col cols="12" sm="12" md="12" xl="12">
-            <v-card :elevation="0" color="transparent" tile class="pb-4 px-0 px-sm-8 px-md-8 px-xl-8" v-for="(feed, idx) in feedst"
-                :key="(idx || feed.link || feed.title)">
+            <v-card :elevation="0" color="transparent" tile class="pb-4 px-0 px-sm-8 px-md-8 px-xl-8"
+                v-for="(feed, idx) in feedst" :key="(idx || feed.link || feed.title)">
 
                 <div class="feed-horizontal-item stacked" :ripple="false" :href="feed.link" target="_blank">
                     <div class="feed-horizontal-content">
@@ -19,15 +20,20 @@
                 </div>
             </v-card>
         </v-col>
-        <v-col cols="12" >
+        <v-col cols="12">
             <v-date-picker v-model="picker" no-title></v-date-picker>
         </v-col>
+        <FollowersComp/>
     </v-row>
 </template>
 
 <script>
+import FollowersComp from "./Followers.vue";
 export default {
     name: 'FeedStackedComp',
+    components: {
+        FollowersComp
+    },
     props: {
         feedst: {
             type: Array,
@@ -35,9 +41,8 @@ export default {
             default: () => []
         }
     },
-
     data: () => ({
         picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
-    })
+    })    
 }
 </script>
