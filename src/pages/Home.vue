b6f7d691389d5b6dcb46b4fe01f2808219ca5ece
obedewardhana <obedewardhana@gmail.com>
2023-02-25 02:47:12 +0700

    Add gallery

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,7 +3,8 @@
         <HomeSectionComp />
         <FeedSectionComp />
         <FaithSectionComp />
-        <InfographicSectionComp />
+        <GallerySectionComp :feeds="news" class="pb-8" />
+        <InfographicSectionComp class="pt-8" />
         <ChurchSectionComp />
         <DonationSectionComp />
     </div>
@@ -13,6 +14,7 @@
 import HomeSectionComp from "@/components/Home/HomeSection.vue";
 import FeedSectionComp from "@/components/Feeds/FeedSection.vue";
 import FaithSectionComp from "@/components/About/FaithSection.vue";
+import GallerySectionComp from "@/components/Gallery/GallerySection.vue";
 import ChurchSectionComp from "@/components/Church/ChurchSection.vue";
 import InfographicSectionComp from "@/components/About/InfographicSection.vue";
 import DonationSectionComp from "@/components/Donation/DonationSection.vue";
@@ -22,12 +24,78 @@ export default {
         HomeSectionComp,
         FeedSectionComp,
         FaithSectionComp,
+        GallerySectionComp,
         InfographicSectionComp,
         ChurchSectionComp,
         DonationSectionComp
     },
     data: () => ({
-        
+        news: [
+            {
+                src: require('@/assets/img/img-pray1.jpg'),
+                title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. 1',
+                link: 'news/detail',
+                description: 'This is the short description for the card purpose.',
+                category: 'informasi',
+                date: '22 Agustus 2022'
+            },
+            {
+                src: require('@/assets/img/img-worship1.jpg'),
+                title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. 2',
+                link: 'news/detail',
+                description: 'This is the short description for the card purpose.',
+                category: 'informasi',
+                date: '22 Agustus 2022'
+            },
+            {
+                src: require('@/assets/img/img-worship2.jpg'),
+                title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. 3',
+                link: 'news/detail',
+                description: 'This is the short description for the card purpose.',
+                category: 'informasi',
+                date: '22 Agustus 2022'
+            },
+            {
+                src: require('@/assets/img/img-pray1.jpg'),
+                title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. 4',
+                link: 'news/detail',
+                description: 'This is the short description for the card purpose.',
+                category: 'informasi',
+                date: '22 Agustus 2022'
+            },
+            {
+                src: require('@/assets/img/img-pray1.jpg'),
+                title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. 1',
+                link: 'news/detail',
+                description: 'This is the short description for the card purpose.',
+                category: 'informasi',
+                date: '22 Agustus 2022'
+            },
+            {
+                src: require('@/assets/img/img-worship1.jpg'),
+                title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. 2',
+                link: 'news/detail',
+                description: 'This is the short description for the card purpose.',
+                category: 'informasi',
+                date: '22 Agustus 2022'
+            },
+            {
+                src: require('@/assets/img/img-worship2.jpg'),
+                title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. 3',
+                link: 'news/detail',
+                description: 'This is the short description for the card purpose.',
+                category: 'informasi',
+                date: '22 Agustus 2022'
+            },
+            {
+                src: require('@/assets/img/img-pray1.jpg'),
+                title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. 4',
+                link: 'news/detail',
+                description: 'This is the short description for the card purpose.',
+                category: 'informasi',
+                date: '22 Agustus 2022'
+            },
+        ],
     }),
     methods: {
     },
@@ -35,8 +103,6 @@ export default {
     }
 };
 </script>
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
   
 
